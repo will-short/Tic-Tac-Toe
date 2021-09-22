4bdf1197a2360934cc8391e7417512f137c706f1
nummyrice <nicholasroyrice@gmail.com>
2021-09-22 14:49:23 -0700

    phase 2 complete

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,1 +1,21 @@
 // Your code here
+
+window.addEventListener("DOMContentLoaded", (e) => {
+  let counter = 0;
+  const sections = document.querySelectorAll("#game > div");
+
+  sections.forEach(section => {
+      section.addEventListener("click", e => {
+          if (!section.innerHTML) {
+              counter++;
+            if (counter % 2 !== 0) {
+                section.innerHTML = "<img src='https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg'>";
+                section.className = "x";
+            } else {
+                section.innerHTML = "<img src='https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg'>";
+                section.className = "o";
+            }
+          }
+      })
+  })
+})
