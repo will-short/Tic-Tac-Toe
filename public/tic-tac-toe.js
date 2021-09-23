cae28175160b1762acc2b084ad7409ec0ac7650f
will short <willofmetal85@gmail.com>
2021-09-23 14:31:29 -0500

    refactored

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,67 +1,73 @@
 // Your code here
-
+let gameWon = false;
 window.addEventListener("DOMContentLoaded", (e) => {
+  let gameBoard = document.getElementById("game");
+  loadBoard(gameBoard);
+  newGame(gameBoard);
+});
+function newGame(gameBoard) {
+  let newGameButton = document.getElementById("new-game");
+  let winText = document.querySelector("h1");
+  newGameButton.addEventListener("click", (e) => {
+    winText.innerText = "";
+    loadBoard(gameBoard);
+  });
+}
+function loadBoard(gameBoard) {
   let counter = 0;
-  const sections = document.querySelectorAll("#game > div");
-
   let matrix = ["", "", "", "", "", "", "", "", ""];
+  let imgX =
+    "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg";
+  let imgY =
+    "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg";
 
-  sections.forEach((section) => {
-    section.addEventListener("click", (e) => {
-      if (!section.innerHTML) {
+  for (let i = 0; i < 9; i++) {
+    let gameDiv = document.createElement("div");
+    gameDiv.id = i;
+
+    gameDiv.addEventListener("click", (e) => {
+      if (!gameDiv.innerHTML && gameWon === false) {
         counter++;
+
         if (counter % 2 !== 0) {
-          matrix[parseInt(section.id)] = "x";
-          section.innerHTML =
-            "<img src='https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg'>";
+          matrix[parseInt(gameDiv.id)] = "x";
+          gameDiv.innerHTML = `<img src=${imgX}>`;
         } else {
-          matrix[parseInt(section.id)] = "o";
-          section.innerHTML =
-            "<img src='https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg'>";
+          matrix[parseInt(gameDiv.id)] = "o";
+          gameDiv.innerHTML = `<img src=${imgY}>`;
         }
-        gameState();
+        gameState(matrix);
       }
     });
-  });
 
-  function gameState() {
-    //checks rows
-
-    if (matrix.every((el) => el === "x" || el === "o"))
-      return winnerText("none");
-
-    for (let i = 0; i < matrix.length - 2; i += 3) {
-      if (matrix.slice(i, i + 3).every((el) => el === "x"))
-        return winnerText("x");
-      if (matrix.slice(i, i + 3).every((el) => el === "o"))
-        return winnerText("o");
-    }
-
-    let col1 = [matrix[0], matrix[3], matrix[6]];
-    let col2 = [matrix[1], matrix[4], matrix[7]];
-    let col3 = [matrix[2], matrix[5], matrix[8]];
-
-    let cols = [col1, col2, col3];
-
-    if (cols.some((col) => col.every((el) => el === "x")))
-      return winnerText("x");
-    if (cols.some((col) => col.every((el) => el === "o")))
-      return winnerText("o");
-
-    let diagLeft = [matrix[0], matrix[4], matrix[8]];
-    let diagRight = [matrix[2], matrix[4], matrix[6]];
-
-    let diags = [diagLeft, diagRight];
-
-    if (diags.some((diag) => diag.every((el) => el === "x")))
-      return winnerText("x");
-    if (diags.some((diag) => diag.every((el) => el === "o")))
-      return winnerText("o");
+    gameBoard.appendChild(gameDiv);
   }
+}
 
-  function winnerText(str) {
-    let winner = document.querySelector("h1");
+function gameState(matrix) {
+  if (matrix.every((el) => el)) return winnerText("none");
 
-    winner.innerText = `Winner: ${str.toUpperCase()}`;
-  }
-});
+  let col1 = matrix[0] + matrix[3] + matrix[6];
+  let col2 = matrix[1] + matrix[4] + matrix[7];
+  let col3 = matrix[2] + matrix[5] + matrix[8];
+
+  let row1 = matrix[0] + matrix[1] + matrix[2];
+  let row2 = matrix[3] + matrix[4] + matrix[5];
+  let row3 = matrix[6] + matrix[7] + matrix[8];
+
+  let diagLeft = matrix[0] + matrix[4] + matrix[8];
+  let diagRight = matrix[2] + matrix[4] + matrix[6];
+
+  let data = [row1, row2, row3, col1, col2, col3, diagLeft, diagRight];
+
+  if (data.some((str) => str === "xxx")) return winnerText("x");
+  if (data.some((str) => str === "ooo")) return winnerText("o");
+}
+
+function winnerText(str) {
+  let winner = document.querySelector("h1");
+
+  winner.innerText = `Winner: ${str.toUpperCase()}`;
+
+  gameWon = true;
+}
