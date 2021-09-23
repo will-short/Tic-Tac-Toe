8a2630364a4473a0225241b39fed8c45816f3208
will short <willofmetal85@gmail.com>
2021-09-23 10:32:11 -0500

    added winner helper

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -25,17 +25,16 @@ window.addEventListener("DOMContentLoaded", (e) => {
   });
 
   function gameState() {
-    let winner = document.querySelector("h1");
     //checks rows
 
     if (matrix.every((el) => el === "x" || el === "o"))
-      winner.innerText = "Winner: None";
+      return winnerText("none");
 
     for (let i = 0; i < matrix.length - 2; i += 3) {
       if (matrix.slice(i, i + 3).every((el) => el === "x"))
-        winner.innerText = "Winner: X";
+        return winnerText("x");
       if (matrix.slice(i, i + 3).every((el) => el === "o"))
-        winner.innerText = "Winner: O";
+        return winnerText("o");
     }
 
     let col1 = [matrix[0], matrix[3], matrix[6]];
@@ -45,10 +44,9 @@ window.addEventListener("DOMContentLoaded", (e) => {
     let cols = [col1, col2, col3];
 
     if (cols.some((col) => col.every((el) => el === "x")))
-      winner.innerText = "Winner: X";
-
+      return winnerText("x");
     if (cols.some((col) => col.every((el) => el === "o")))
-      winner.innerText = "Winner: O";
+      return winnerText("o");
 
     let diagLeft = [matrix[0], matrix[4], matrix[8]];
     let diagRight = [matrix[2], matrix[4], matrix[6]];
@@ -56,9 +54,14 @@ window.addEventListener("DOMContentLoaded", (e) => {
     let diags = [diagLeft, diagRight];
 
     if (diags.some((diag) => diag.every((el) => el === "x")))
-      winner.innerText = "Winner: X";
-
+      return winnerText("x");
     if (diags.some((diag) => diag.every((el) => el === "o")))
-      winner.innerText = "Winner: O";
+      return winnerText("o");
+  }
+
+  function winnerText(str) {
+    let winner = document.querySelector("h1");
+
+    winner.innerText = `Winner: ${str.toUpperCase()}`;
   }
 });
