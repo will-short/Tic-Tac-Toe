02c6ddf14b2ef7ed80c37b1c7d714f43189e7bd1
will short <willofmetal85@gmail.com>
2021-09-24 11:19:37 -0500

    computer player works v1

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -1,108 +1,80 @@
-// Your code here
+//game meta data
 let gameWon = 0;
 let matrix = ["", "", "", "", "", "", "", "", ""];
 let counter = 1;
+let computerPlayer = currentPlayer(Math.floor(Math.random() * (3 - 1) + 1));
+let you = computerPlayer === "x" ? "O" : "X";
+//make docucalls
 
 window.addEventListener("DOMContentLoaded", (e) => {
-  let gameBoard = document.getElementById("game");
-  let localMain = localStorage.getItem("main");
-  let localCounter = localStorage.getItem("counter");
-  let localMatrix = localStorage.getItem("matrix");
-  let localWinState = localStorage.getItem("winState");
-
-  if (localMain) {
-    console.log("works");
-    main.innerHTML = localMain;
-    counter = localCounter;
-    matrix = localMatrix.split(",");
-    gameWon = parseInt(localWinState);
-  } else {
-    for (let i = 0; i < 9; i++) {
-      let gameDiv = document.createElement("div");
-      gameDiv.id = i;
-      gameBoard.appendChild(gameDiv);
-    }
-  }
-  loadBoard(gameBoard);
-  setLocalStorage();
-  newGame(gameBoard);
+  if (localStorage.getItem("main")) getLocalStorage();
+  loadBoard();
+  newGame();
   giveUp();
 });
 
-function setLocalStorage() {
-  let main = document.getElementById("main");
-  localStorage.setItem("main", main.innerHTML);
-  localStorage.setItem("counter", counter);
-  localStorage.setItem("matrix", matrix);
-  localStorage.setItem("winState", gameWon);
-}
-
 function giveUp() {
   let giveUpButton = document.getElementById("give-up");
+  if (gameWon === 1) giveUpButton.disabled = true;
 
   giveUpButton.addEventListener("click", (e) => {
-    if (currentPlayer() === "x") {
-      winnerText("o");
+    if (currentPlayer(counter) === "x") {
+      winner("o");
     } else {
-      winnerText("x");
+      winner("x");
     }
     giveUpButton.disabled = true;
   });
 }
 
-function newGame(gameBoard) {
-  let giveUpButton = document.getElementById("give-up");
+function newGame() {
   let newGameButton = document.getElementById("new-game");
-  let winText = document.querySelector("h1");
-  let gameSections = document.querySelectorAll("#game > div");
-
   if (gameWon === 0) newGameButton.disabled = true;
-
-  newGameButton.addEventListener("click", (e) => {
-    giveUpButton.disabled = false;
-    newGameButton.disabled = true;
-    gameWon = 0;
-    winText.innerText = "";
-    matrix = ["", "", "", "", "", "", "", "", ""];
-    gameSections.forEach((gameSection) => (gameSection.innerHTML = ""));
-    counter = 1;
-    setLocalStorage();
-  });
+  newGameButton.addEventListener("click", resetGame);
 }
 
-function currentPlayer() {
-  if (counter % 2 === 0) return "o";
-  return "x";
+function computer() {
+  if (gameWon !== 0) return;
+  let gameChildren = document.querySelectorAll("#game > div:not(.played)");
+  let randDiv = Math.floor(Math.random() * gameChildren.length);
+
+  if (computerPlayer === "x" && counter % 2 !== 0) {
+    action(gameChildren[randDiv]);
+  } else if (computerPlayer === "o" && counter % 2 === 0) {
+    action(gameChildren[randDiv]);
+  }
 }
 
-function loadBoard(gameBoard) {
-  let imgX =
-    "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg";
-  let imgY =
-    "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg";
+function setPlayers() {
+  let player = document.getElementById("you");
+  let computerP = document.getElementById("computer");
+
+  player.innerText = `You: ${you}`;
+  computerP.innerText = `computer: ${computerPlayer.toUpperCase()}`;
+}
+
+function loadBoard() {
+  you = computerPlayer === "x" ? "O" : "X";
+  let wait = (seconds) => new Promise((res) => setTimeout(res, seconds * 1000));
+
+  setPlayers();
 
   let gameChildren = document.querySelectorAll("#game > div");
-
+  computer();
   gameChildren.forEach((gameDiv) => {
     gameDiv.addEventListener("click", (e) => {
-      if (!gameDiv.innerHTML && gameWon === 0) {
-        if (currentPlayer() === "x") {
-          matrix[parseInt(gameDiv.id)] = "x";
-          gameDiv.innerHTML = `<img src=${imgX}>`;
-        } else {
-          matrix[parseInt(gameDiv.id)] = "o";
-          gameDiv.innerHTML = `<img src=${imgY}>`;
-        }
-        counter++;
-        setLocalStorage();
-        gameState(matrix);
+      if (you === "X" && counter % 2 !== 0) {
+        action(gameDiv);
+      } else if (you === "O" && counter % 2 === 0) {
+        action(gameDiv);
       }
+      wait(0.3).then(computer);
     });
   });
 }
 
-function gameState(matrix) {
-  if (matrix.every((el) => el)) return winnerText("none");
+function gameState() {
+  if (matrix.indexOf("") < 0) winner("none");
 
   let col1 = matrix[0] + matrix[3] + matrix[6];
   let col2 = matrix[1] + matrix[4] + matrix[7];
@@ -117,11 +89,12 @@ function gameState(matrix) {
 
   let data = [row1, row2, row3, col1, col2, col3, diagLeft, diagRight];
 
-  if (data.some((str) => str === "xxx")) return winnerText("x");
-  if (data.some((str) => str === "ooo")) return winnerText("o");
+  if (data.indexOf("xxx") >= 0) return winner("x");
+  if (data.indexOf("ooo") >= 0) return winner("o");
+  setLocalStorage();
 }
 
-function winnerText(str) {
+function winner(str) {
   let newGameButton = document.getElementById("new-game");
 
   let winner = document.querySelector("h1");
@@ -130,4 +103,82 @@ function winnerText(str) {
   newGameButton.disabled = false;
   gameWon = 1;
   setLocalStorage();
+}
+
+function currentPlayer(num) {
+  if (num % 2 === 0) return "o";
+  return "x";
+}
+
+function action(gameDiv) {
+  let giveUpButton = document.getElementById("give-up");
+  let imgX =
+    "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg";
+  let imgY =
+    "https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg";
+
+  if (!gameDiv.innerHTML && gameWon === 0) {
+    if (currentPlayer(counter) === "x") {
+      matrix[parseInt(gameDiv.id)] = "x";
+      gameDiv.className = "played";
+      gameDiv.innerHTML = `<img src=${imgX}>`;
+    } else {
+      matrix[parseInt(gameDiv.id)] = "o";
+      gameDiv.className = "played";
+      gameDiv.innerHTML = `<img src=${imgY}>`;
+    }
+
+    counter++;
+    gameState();
+    if (gameWon === 1) giveUpButton.disabled = true;
+  }
+}
+
+function resetGame() {
+  let giveUpButton = document.getElementById("give-up");
+  let winText = document.querySelector("h1");
+  let gameSections = document.querySelectorAll("#game > div");
+  let newGameButton = document.getElementById("new-game");
+
+  giveUpButton.disabled = false;
+  newGameButton.disabled = true;
+  gameWon = 0;
+  winText.innerText = "";
+  matrix = ["", "", "", "", "", "", "", "", ""];
+
+  gameSections.forEach((gameSection) => {
+    gameSection.innerHTML = "";
+    gameSection.classList.remove("played");
+  });
+  counter = 1;
+
+  computerPlayer = currentPlayer(Math.floor(Math.random() * (3 - 1) + 1));
+  you = computerPlayer === "x" ? "O" : "X";
+  computer(computerPlayer);
+
+  setPlayers();
+  setLocalStorage();
+}
+
+function getLocalStorage() {
+  let localMain = localStorage.getItem("main");
+  let localCounter = localStorage.getItem("counter");
+  let localMatrix = localStorage.getItem("matrix");
+  let localWinState = localStorage.getItem("winState");
+  let localComputer = localStorage.getItem("computer");
+
+  computerPlayer = localComputer;
+  main.innerHTML = localMain;
+  counter = localCounter;
+  matrix = localMatrix.split(",");
+  gameWon = parseInt(localWinState);
+}
+
+function setLocalStorage() {
+  let main = document.getElementById("main");
+  localStorage.setItem("computer", computerPlayer);
+  localStorage.setItem("main", main.innerHTML);
+  localStorage.setItem("counter", counter);
+  localStorage.setItem("matrix", matrix);
+  localStorage.setItem("winState", gameWon);
 }
